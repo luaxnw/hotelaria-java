5fe5f581dd0e88fa588e27c28b469d2e2e2fbc31
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-06-02 21:56:07 -0300

    Adiciona especificação 4.

--- Reservas.java
+++ Reservas.java
@@ -5,7 +5,81 @@ public class Reservas {
     private int codigo;
     private Hospede hospedeResponsavel;
     private Acomodacoes acomodacao;
-    private ArrayList<Hospede> listaHospede;
     private int quantidadeHospedes;
+    private int qtdDias;
+    private ArrayList<Servicos> servicos;
     
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Hospede getHospedeResponsavel() {
+        return hospedeResponsavel;
+    }
+
+    public void setHospedeResponsavel(Hospede hospedeResponsavel) {
+        this.hospedeResponsavel = hospedeResponsavel;
+    }
+
+    public Acomodacoes getAcomodacao() {
+        return acomodacao;
+    }
+
+    public void setAcomodacao(Acomodacoes acomodacao) {
+        this.acomodacao = acomodacao;
+    }
+
+    public int getQuantidadeHospedes() {
+        return quantidadeHospedes;
+    }
+
+    public void setQuantidadeHospedes(int quantidadeHospedes) {
+        this.quantidadeHospedes = quantidadeHospedes;
+    }
+
+    public int getQtdDias() {
+        return qtdDias;
+    }
+
+    public void setQtdDias(int qtdDias) {
+        this.qtdDias = qtdDias;
+    }
+
+    public ArrayList<Servicos> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(ArrayList<Servicos> servicos) {
+        this.servicos = servicos;
+    }
+
+    public void addServico(Servicos servicos) {
+        if (this.servicos == null) {
+            this.servicos = new ArrayList<>();
+        }
+        this.servicos.add(servicos);
+    }
+
+    public double calcularPrecoReserva(){
+        double somaValorTotal = 0;
+        for (int i = 0; i < servicos.size(); i++){
+            somaValorTotal += servicos.get(i).calcularValor();
+        }
+        somaValorTotal =+ acomodacao.calculaPrecoTotalDiaria();
+        
+        return somaValorTotal;
+    }
+
+
+
+
+
+
+
+
+
 }
