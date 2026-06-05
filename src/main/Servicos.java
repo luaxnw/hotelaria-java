16f6cc5b4ea243b1fe332256a1927275c9d3cffb
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-06-05 11:00:20 -0300

    Corrige métodos sets da classe serviços

--- src/main/Servicos.java
+++ src/main/Servicos.java
@@ -3,24 +3,32 @@ public abstract class Servicos {
     private int codigo;
     private double valorBase;
 
-    public int getCodigo(){
+    public int getCodigo() {
         return codigo;
     }
-    public void setCodigo(int codigo){
-        this.codigo = codigo;
+
+    public void setCodigo(int codigo) {
+        if (codigo > 0)
+            this.codigo = codigo;
+        else {
+            throw new IllegalArgumentException("O código de serviço deve ser maior que zero");
+        }
     }
 
-    public double getValorBase(){
+    public double getValorBase() {
         return valorBase;
     }
 
-    public void setValorBase(double valorBase){
-        this.valorBase = valorBase;
+    public void setValorBase(double valorBase) {
+        if (valorBase > 0)
+            this.valorBase = valorBase;
+        else {
+            throw new IllegalArgumentException("O valor base deve ser maior que zero");
+        }
     }
 
     public abstract void mostraDados();
+
     public abstract double calcularValor();
 
-
-    
 }
