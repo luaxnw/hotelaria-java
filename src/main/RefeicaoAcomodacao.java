404e172b6a77b3ecef5da7e54af8340d85e3dc70
luaxnw <luan.pescador@estudante.uffs.edu.br>
2026-06-12 10:53:54 -0300

    Adiciona construtor para classe RefeicaoAcomodacao

--- src/main/RefeicaoAcomodacao.java
+++ src/main/RefeicaoAcomodacao.java
@@ -1,5 +1,9 @@
 public class RefeicaoAcomodacao extends Servicos {
 
+    public RefeicaoAcomodacao(int id, double valorBase) {
+        super(id, valorBase);
+    }
+
     @Override
     public void mostraDados(){
         System.out.println("Tipo de Serviço: Refeição na acomodação\nValor: " + getValorBase());
